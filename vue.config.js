aa066761d7af2c97d9a3353b3ae9c8ddfbd717cf
Kitty2sh1 <2747993265@qq.com>
2022-09-29 09:23:35 +0800

    feat(custom): 项目基础配置

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer:{
+    // 是否自动打开浏览器
+    open:true,
+    // 是否开启https
+    https:false,
+    // 主机名
+    host:"localhost"
+  }
 })
